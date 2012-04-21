2ddc97ff281585e131a2e2faf3285e356ea0fc10
James Turnbull <james@incuna.com>
2012-04-21 14:40:28 +0100

    Added mongodb

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import requests
 
 from flask import Flask
+from flaskext.mongoalchemy import MongoAlchemy
 
 ENDPOINT = 'http://data.nasa.gov/api/'
 
 app = Flask(__name__)
+app.config['MONGOALCHEMY_DATABASE'] = 'nasadata'
+db = MongoAlchemy(app)
+
+class Dataset(db.Document):
+    remote_id = db.IntField()
+    data = db.StringField()
 
 @app.route('/', methods=['GET'])
 def index():
 
     response = requests.get(ENDPOINT + 'get_dataset?id=354')
-    print(response.text)
+    response_text = response.text
+    dataset = Dataset(remote_id = response.text.id, data=response.text)
+    dataset.save()
+    
+    
 
 if __name__ == '__main__':
     app.debug = True
