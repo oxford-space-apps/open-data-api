acbace21652d45a5fb17bbb487692ab6057313df
George Hickman <george@ghickman.co.uk>
2012-04-21 16:43:19 +0100

    Define a query on Dataset to retrive them by remote id

--- api/parsers/datanasa.py
+++ api/parsers/datanasa.py
@@ -1,5 +1,5 @@
 import json
-from flaskext.mongoalchemy import MongoAlchemy
+from flaskext.mongoalchemy import BaseQuery
 import requests
 
 from api import app
@@ -7,6 +7,12 @@ from api import db
 
 
 ENDPOINT = 'http://data.nasa.gov/api/'
+
+
+class DatasetQuery(BaseQuery):
+    def get_by_remote_id(self, pk):
+        return self.filter(self.type.remote_id==pk)
+
 
 class Dataset(db.Document):
     """ Represents a dataset,
@@ -16,8 +22,12 @@ class Dataset(db.Document):
     remote_id = db.IntField()
     data = db.StringField()
 
+    query_class = DatasetQuery
+
+
 def get_dataset(id):
     response = requests.get(ENDPOINT + 'get_dataset?id=%s' % id)
     dataset_data = json.loads(response.text)
     dataset = Dataset(remote_id = id, data=response.text)
     dataset.save()
+
