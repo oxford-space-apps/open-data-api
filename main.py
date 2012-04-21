de7e4ee53e71e9502f62dd97f3160495c0220826
James Turnbull <james@incuna.com>
2012-04-21 14:30:01 +0100

    example

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import requests
+
 from flask import Flask
 
+ENDPOINT = 'http://data.nasa.gov/api/'
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello():
-    return 'Hello World!'
+def index():
 
+    response = requests.get(ENDPOINT + 'get_dataset?id=354')
+    print(response.text)
 
 if __name__ == '__main__':
     app.debug = True
